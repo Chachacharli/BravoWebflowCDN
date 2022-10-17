f19094f69d34f1b100e90fd6246f7a133ae9b9dc
Carlos Alberto Torres Cano <84156291+Chachacharli@users.noreply.github.com>
2022-10-16 19:23:27 -0500

    Add files via upload

---
+++ layoutGeometria/layoutGeometria.js
@@ -0,0 +1,10 @@
+
+var geometrias = ['Endurance', 'Allrounder', 'Race']
+
+var elem = document.querySelector('#geometriaBici');
+
+var rangeValue = function(){
+  var newValue = elem.value;
+  var target = document.querySelector('.geometria');
+  target.innerHTML = geometrias[newValue];
+}
